3342c7e1947dd4ae58129991bc2c27a72bdab4fd
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2021-04-12 00:27:53 -0300

    fix(components): fix ref bug :rocket:

--- src/Components/Atoms/Form/index.tsx
+++ src/Components/Atoms/Form/index.tsx
@@ -7,12 +7,14 @@ interface IProps extends Omit<FormProps, 'ref'> {
   ref?: Ref<FormHandles>;
 }
 
-const Form: React.FC<IProps> = ({ children, ...rest }) => {
-  return (
-    <BaseForm {...rest}>
-      <Stack spacing="40px">{children}</Stack>
-    </BaseForm>
-  );
-};
+const Form: React.FC<IProps> = React.forwardRef(
+  ({ children, ...rest }, ref) => {
+    return (
+      <BaseForm ref={ref} {...rest}>
+        <Stack spacing="40px">{children}</Stack>
+      </BaseForm>
+    );
+  },
+);
 
 export default Form;
