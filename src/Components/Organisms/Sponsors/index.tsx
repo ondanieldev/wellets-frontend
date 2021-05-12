6eb3a9ce0deff1e962a32bd2efd421694e000f54
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2021-05-11 21:07:20 -0300

    feat(sponsors): add info :rocket:

--- src/Components/Organisms/Sponsors/index.tsx
+++ src/Components/Organisms/Sponsors/index.tsx
@@ -1,16 +1,57 @@
 import React from 'react';
+import { FiGithub } from 'react-icons/fi';
 
-import { Box } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Icon,
+  LinkBox,
+  LinkOverlay,
+  Text,
+} from '@chakra-ui/react';
 
 const Sponsors: React.FC = () => {
   return (
-    <Box
-      h="100%"
-      w="100%"
-      maxW="400px"
-      bg="green.300"
-      borderStartRadius="5px"
-    />
+    <Box h="100%" w="100%" maxW="400px" bg="green.300" borderStartRadius="5px">
+      <Flex
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+      >
+        <Flex
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          flex="1"
+        >
+          <Heading color="gray.700" size="2xl">
+            Wellets
+          </Heading>
+          <Heading color="gray.700" size="md">
+            v1.0
+          </Heading>
+        </Flex>
+
+        <Box>
+          <LinkBox w="100%" h="100%">
+            <Flex
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Icon as={FiGithub} height={25} width={25} color="gray.700" />
+              <Text color="gray.700" fontWeight="bold">
+                <LinkOverlay href="https://github.com/stemDaniel/wellets-frontend">
+                  GitHub
+                </LinkOverlay>
+              </Text>
+            </Flex>
+          </LinkBox>
+        </Box>
+      </Flex>
+    </Box>
   );
 };
 
